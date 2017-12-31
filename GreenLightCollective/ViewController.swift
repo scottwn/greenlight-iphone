d0c1497c1af1f9175e206bcac054f32b3c416f14
Scott Wyman Neagle <scott.neagle@cs.nyu.edu>
2017-12-31 12:10:41 -0500

    Initial Commit

---
+++ GreenLightCollective/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GreenLightCollective
+//
+//  Created by Scott Neagle on 12/31/17.
+//  Copyright © 2017 Scott Neagle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
